f81d2563c225caf00fd38079c6b5d8126890e72c
Benjamin Wright <bwright@trilogyed.com>
2020-02-24 15:43:03 -0500

    added db name to config

--- config/config.js
+++ config/config.js
@@ -2,7 +2,7 @@ module.exports = {
   "development": {
     "username": process.env.DB_USER,
     "password": process.env.DB_PASS,
-    "database": "database_development",
+    "database": process.env.DB_NAME,
     "host": "127.0.0.1",
     "dialect": "mysql",
     "operatorsAliases": false
