b0e465055eba7450212c03460b76526c2c459b52
Benjamin Wright <bwright@trilogyed.com>
2020-02-24 15:50:05 -0500

    Setup link to apiRoutes

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 require('dotenv').config();
 const express = require('express');
+const apiRoutes = require('./routes/apiRoutes');
+
 const app = express();
 
 const PORT = process.env.PORT || 8000;
+
+app.use('/api', apiRoutes);
 
 app.get('*', (req,res) => {
   res.send('Your page not found. But, thanks for visiting.')
