5a109afc9f1502452ceecc6f334952bce82253ac
Benjamin Wright <bwright@trilogyed.com>
2020-02-24 15:47:28 -0500

    setup base server

--- server.js
+++ server.js
@@ -1,1 +1,13 @@
 require('dotenv').config();
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 8000;
+
+app.get('*', (req,res) => {
+  res.send('Your page not found. But, thanks for visiting.')
+});
+
+app.listen(PORT, () => {
+  console.log(`You runnin on ${PORT}`);
+})
