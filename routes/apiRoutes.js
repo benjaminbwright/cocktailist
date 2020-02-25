1f869973a4217b9c0ebc74fd89c95ecba3096180
Benjamin Wright <bwright@trilogyed.com>
2020-02-25 16:54:10 -0500

    Setup base cocktail crud

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -34,8 +34,45 @@ router.delete("/ingredients/:id", async ({params}, res) => {
 })
 
 // Cocktail Routes ========================================
-router.get('/cocktails', (req, res) => {
-  res.send('This will be all the cocktials in json format')
+
+// Create Cocktails
+router.post("/cocktails", async ({body}, res) => {
+  // create the new cocktail
+  const addedCocktail = await db.Cocktail.create(body);
+  addedCocktail.addIngredients(body.ingredients);
+  res.json(addedCocktail);
+});
+// Read Cocktails
+router.get("/cocktails", async (req, res) => {
+  const cocktails = await db.Cocktail.findAll({
+    include: [db.Ingredient]
+  });
+  res.json(cocktails);
+});
+// Update Cocktails
+router.put("/cocktails/:id", async ({body, params}, res) => {
+  const numUpdated = await db.Cocktail.update(body, {
+    where: {
+      id: params.id
+    }
+  });
+  const cocktail = await db.Cocktail.findOne({
+    where: {
+      id: params.id
+    }
+  });
+  cocktail.setIngredients(body.ingredients);
+
+  res.json(numUpdated);
+});
+// Delete Cocktails
+router.delete("/cocktails/:id", async ({params}, res) => {
+  const numDeleted = await db.Cocktail.destroy({
+    where: {
+      id: params.id
+    }
+  })
+  res.json(numDeleted);
 })
 
 module.exports = router;
